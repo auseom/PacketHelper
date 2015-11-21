1e6c2a5750f876f8f03321f2a01857e21e1a13e9
auseom <secugyu@gmail.com>
2015-11-21 18:51:17 +0900

    Add tooltip for hided item in sensitive Data ListView and update About.cs

--- Packet Helper/About.Designer.cs
+++ Packet Helper/About.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.textBox_about = new System.Windows.Forms.TextBox();
             this.button_close = new System.Windows.Forms.Button();
+            this.label_visitGibhub = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_about
@@ -46,7 +47,7 @@
             // 
             // button_close
             // 
-            this.button_close.Location = new System.Drawing.Point(197, 226);
+            this.button_close.Location = new System.Drawing.Point(197, 236);
             this.button_close.Name = "button_close";
             this.button_close.Size = new System.Drawing.Size(75, 23);
             this.button_close.TabIndex = 1;
@@ -54,11 +55,24 @@
             this.button_close.UseVisualStyleBackColor = true;
             this.button_close.Click += new System.EventHandler(this.button_close_Click);
             // 
+            // label_visitGibhub
+            // 
+            this.label_visitGibhub.AutoSize = true;
+            this.label_visitGibhub.Location = new System.Drawing.Point(10, 223);
+            this.label_visitGibhub.Name = "label_visitGibhub";
+            this.label_visitGibhub.Size = new System.Drawing.Size(65, 12);
+            this.label_visitGibhub.TabIndex = 2;
+            this.label_visitGibhub.Text = "initial label";
+            this.label_visitGibhub.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label_visitGibhub_MouseClick);
+            this.label_visitGibhub.MouseLeave += new System.EventHandler(this.label_visitGibhub_MouseLeave);
+            this.label_visitGibhub.MouseHover += new System.EventHandler(this.label_visitGibhub_MouseHover);
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 271);
+            this.Controls.Add(this.label_visitGibhub);
             this.Controls.Add(this.button_close);
             this.Controls.Add(this.textBox_about);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -76,5 +90,6 @@
 
         private System.Windows.Forms.TextBox textBox_about;
         private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Label label_visitGibhub;
     }
 }
